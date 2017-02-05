909f0b524afbff3796e38fb01920261621d09fd2
Sajay Bailey <sajay.bailey@mymona.uwi.edu>
2017-02-05 02:51:20 +0000

    completed commit

--- app/views.py
+++ app/views.py
@@ -7,7 +7,17 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
+import time
 
+
+###
+#method to get time
+###
+
+def timeinfo ():
+    return time.strftime("%a, %d %b %Y")
+    
+    
 
 ###
 # Routing for your application.
@@ -23,6 +33,12 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+    
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page"""
+    return render_template('profile.html', today = timeinfo())
 
 
 ###
